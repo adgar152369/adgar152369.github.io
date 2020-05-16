313a0b540cf8a6ae1c6873124b363fa599d6ea32
adgar152369 <gar15041@byui.edu>
2020-05-15 23:40:19 -0600

    commit

--- lesson4/js/style.js
+++ lesson4/js/style.js
@@ -1,3 +1,7 @@
 function toggle_menu(){
     document.getElementById('navigation').classList.toggle('hide');
 }
+
+var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+document.getElementById('today-date').innerHTML = date;
