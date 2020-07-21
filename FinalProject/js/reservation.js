0c911c485ad914ca7619478b77a8f411f7e9e551
adgar152369 <gar15041@byui.edu>
2020-07-21 17:51:36 -0600

    commit

---
+++ FinalProject/js/reservation.js
@@ -0,0 +1,11 @@
+const url = "https://mfuller45.github.io/final/data/scoots.json";
+fetch(url)
+  .then((response) => response.json())
+    .then((data) => {
+        data.rentals.forEach((rental, index) => {
+            document.getElementById('rhd' + (index + 1)).innerHTML = rental.half1;
+            document.getElementById('rfd' + (index + 1)).innerHTML = rental.full1;
+            document.getElementById('whd' + (index + 1)).innerHTML = rental.half2;
+            document.getElementById('wfd' + (index + 1)).innerHTML = rental.full2;
+        });
+});
