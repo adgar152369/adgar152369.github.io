03adbcb9f323a93b9a45b0e5dfaf7e6bb57e351a
adgar152369 <gar15041@byui.edu>
2020-10-14 17:57:11 -0600

    commit

--- lesson5/js/banner.js
+++ lesson5/js/banner.js
@@ -11,15 +11,7 @@ var dates = [
 var date = new Date();
 var day = dates[date.getDay()];
 
-if (day == "Sunday"){
-    document.getElementById('banner-display').innerHTML = "Today is " + day + "!";
-}
-if (day == "Monday"){
-    document.getElementById('banner-display').innerHTML = "Today is " + day + "!";
-}
-if (day == "Tuesday"){
-    document.getElementById('banner-display').innerHTML = "<b>"+ day + "</b> = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion!";
-}
-if (day == "Wednesday"){
-    document.getElementById('banner-display').innerHTML = "Today is " + day + "!";
+
+if (day == "Friday"){
+    document.getElementById('banner-display').innerHTML = "<b>" + day + "</b> = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
 }
