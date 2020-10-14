2e6aa73be4c5838b0e81cfe5195613c1ccbdfcec
adgar152369 <gar15041@byui.edu>
2020-10-14 17:58:08 -0600

    commit

--- lesson5/js/banner.js
+++ lesson5/js/banner.js
@@ -12,6 +12,6 @@ var date = new Date();
 var day = dates[date.getDay()];
 
 
-if (day == "Friday"){
+if (day == "Wednesday"){
     document.getElementById('banner-display').innerHTML = "<b>" + day + "</b> = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
 }
